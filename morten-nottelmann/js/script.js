5d13b148f469703dcaa700b51d6efadaac77ec0e
cplovst <plovst@live.dk>
2018-09-06 22:08:05 +0200

    kilde

--- morten-nottelmann/js/script.js
+++ morten-nottelmann/js/script.js
@@ -1,5 +1,5 @@
 
-//
+// Med inspiration fra https://stackoverflow.com/questions/21163756/html5-and-javascript-to-play-videos-only-when-visible
 var video = document.getElementById('myVideo'), fraction = 0.8;
 
             function checkScroll() {
